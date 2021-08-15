5ea7d4280ecc6a08b9af4a76801673b19a661fe2
jayrfs <rfsjay@gmail.com>
2021-08-15 13:22:37 +0530

    ➕ Dependancy: Kivy 💄 Add: GUI (initial commit)

---
+++ stitcher_gui.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.config import Config
+from kivy.uix.filechooser import FileChooserListView
+
+Config.set('graphics', 'width', '600')
+Config.set('graphics', 'height', '450')
+Config.write()
+
+#main window
+class Stitcher(App):
+    def build(self):
+        layout = GridLayout(
+                cols = 1,
+                rows = 2,
+                rows_minimum = {1:200,2:50},
+                spacing = 10,
+                #padding = 10,
+                pos_hint = {"center_x":0.5,
+                            "center_y":0.5
+                            }
+                )
+        layout.add_widget(Label(text='Stitcher',  font_size='50sp'))
+        layout.add_widget(FileChooserListView())
+        #layout.add_widget(Label(text='Body',  font_size='50sp'))
+        return(layout)
+
+Stitcher().run()
